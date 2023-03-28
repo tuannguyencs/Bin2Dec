3ede40d09845748c78de5af5741ec29756cdcc49
tuannguyencs <ttn079@gmail.com>
2023-03-28 16:03:55 -0500

    Initial commit

---
+++ Bin2Dec/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    // On document ready, attach a click event listener to the "Convert" button
+    $('#convert').click(function () {
+        // Get the binary input value
+        var binaryStr = $('#binary').val();
+
+        // Convert binary to decimal
+        var decimalNum = parseInt(binaryStr, 2);
+
+        // Set the decimal output value
+        $('#decimal').val(decimalNum);
+    });
+});
